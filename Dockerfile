df3ddaed73c702f1ac3124a791d3b62eaf8d9b80
Zhouhao12345 <zhouhao19931002@163.com>
2018-04-18 16:40:28 +0800

    [IMP]replace flask to grpc

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,7 @@
-FROM caffe:cpu
-EXPOSE 5000
+FROM mxmcherry/yahoo-open-nsfw-http:latest
+EXPOSE 50052
 
-RUN pip install Flask
-
-RUN wget 'https://raw.githubusercontent.com/yahoo/open_nsfw/master/nsfw_model/deploy.prototxt' -O 'model_def.prototxt'
-RUN wget 'https://github.com/yahoo/open_nsfw/raw/master/nsfw_model/resnet_50_1by2_nsfw.caffemodel' -O 'pretrained_model.caffemodel'
-
-COPY http.py /workspace/
-
-ENV MODEL_DEF model_def.prototxt
-ENV PRETRAINED_MODEL pretrained_model.caffemodel
-ENV HOST 0.0.0.0
-ENV PORT 5000
-ENV MAX_CONTENT_LENGTH 16777216
+RUN pip install grpcio -i http://pypi.douban.com/simple/ saltTesting --trusted-host pypi.douban.com
+RUN rm -f http.py
 
 CMD python http.py
--- grpc_server/http.py
+++ grpc_server/http.py
@@ -2,17 +2,25 @@
 
 import numpy
 import PIL
+from PIL import Image
 import caffe
-import io
-import flask
+from io import BytesIO
 import os
+import grpc
+import data_pb2
+import data_pb2_grpc
+import time
+import base64
+from concurrent import futures
+
+_ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
 def resize_image(image_stream, sz=(256, 256)):
     im = PIL.Image.open(image_stream)
     if im.mode != 'RGB':
         im = im.convert('RGB')
     imr = im.resize(sz, resample=PIL.Image.BILINEAR)
-    imrh = io.BytesIO()
+    imrh = BytesIO()
     imr.save(imrh, format='JPEG')
     imrh.seek(0)
     return imrh
@@ -48,31 +56,28 @@ def make_transformer(net):
     transformer.set_channel_swap('data', (2, 1, 0))             # swap channels from RGB to BGR
     return transformer
 
-def make_app(net, transformer):
-    app = flask.Flask(__name__)
+class Greeter(data_pb2_grpc.GreeterServicer):
 
-    @app.route('/', methods=['POST'])
-    def net_score():
-        image_data = flask.request.get_data()
-        image_stream = io.BytesIO(image_data)
-        score = score_image(image_stream, net=net, transformer=transformer)
-        return str(score)
-
-    return app
+    def CheckImageFace(self, request, context):
+        model_def = os.getenv('MODEL_DEF', 'model_def.prototxt')
+        pretrained_model = os.getenv('PRETRAINED_MODEL', 'pretrained_model.caffemodel')
+        img_str = base64.b64decode(request.imageBuffer)
+        net = caffe.Net(model_def, pretrained_model, caffe.TEST)
+        transformer = make_transformer(net)
+        buffer = BytesIO(img_str)
+        score = score_image(buffer, net=net, transformer=transformer)
+        return data_pb2.CheckReply(checkResult=str(score))
 
 def main():
-    model_def          = os.getenv('MODEL_DEF', 'model_def.prototxt')
-    pretrained_model   = os.getenv('PRETRAINED_MODEL', 'pretrained_model.caffemodel')
-    host               = os.getenv('HOST', '0.0.0.0')
-    port               = os.getenv('PORT', '5000')
-    max_content_length = os.getenv('MAX_CONTENT_LENGTH', str(16 * 1024 * 1024))
-
-    net         = caffe.Net(model_def, pretrained_model, caffe.TEST)
-    transformer = make_transformer(net)
-
-    app = make_app(net, transformer)
-    app.config['MAX_CONTENT_LENGTH'] = max_content_length
-    app.run(host=host, port=port)
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    data_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
+    server.add_insecure_port('[::]:50052')
+    server.start()
+    try:
+        while True:
+            time.sleep(_ONE_DAY_IN_SECONDS)
+    except KeyboardInterrupt:
+        server.stop(0)
 
 if __name__ == '__main__':
     main()
